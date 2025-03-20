bf7e1d48bf47949d3d6847ee3075dc108cdb1cc4
Adrian-Dz <dzieladrian@gmail.com>
2025-03-20 23:03:52 +0100

    Base

--- Main/Backend/Api.go
+++ Main/Backend/Api.go
@@ -1,10 +1,41 @@
-package Backend
+package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
 )
 
+type Pos struct {
+}
+
+var Positions []Pos
+
+func GameInput(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+	}
+}
+
+func GameGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+
+	}
+}
+
+func GameDelete(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "DELETE" {
+
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Print("Enter your name: ")
+	router := chi.NewRouter()
+
+	http.HandleFunc("/Game/Post", GameInput)
+	http.HandleFunc("Game/Get", GameGet)
+	http.HandleFunc("/Game/Delete", GameDelete)
+
+	fmt.Print("Server is running")
+	http.ListenAndServe(":25025", router)
 }
--- Main/Backend/go.mod
+++ Main/Backend/go.mod
@@ -1,3 +1,5 @@
-module Backend
+module main
 
-go 1.24
+go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
